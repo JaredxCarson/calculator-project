fded0d5037e3b6ad6da5374f4a5f76e5122eb2b6
Jared <jchris1715@gmail.com>
2024-10-21 12:11:25 -0400

    called buttons and implemented operator function

--- calculator-project.js
+++ calculator-project.js
@@ -1,3 +1,20 @@
+//Created 3 different variables for the first #, operator and the second #
+let currentOperand = '';
+let previousOperand = '';
+let operator = null;
+
+//Calling all of the Calculator buttons and the display element
+//We use querySelectorAll() to select multiple elements that share the same class or attribute
+//And then getElementById() is for when you want to select a single unique element
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalsButton = document.getElementById('equals');
+const clearButton = document.getElementById('clear');
+const deleteButton = document.getElementById('delete');
+const display = document.getElementById('display');
+
+
+
 //created functions for the operator logic
 function add(a, b) {
     return a + b;
@@ -17,3 +34,20 @@ function divide(a, b) {
     } return a / b;
 }
 //Cant divide a number by 0 so we set it to return an error
+
+
+//Function To Perform Operation
+function operate(operator, a, b) {
+    switch(operator) {
+        case '+':
+            return add(a,b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return null;               
+    }
+}
